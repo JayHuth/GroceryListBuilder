0a4b0b7b8350bada0b3f8ddecc6e3aee2b522609
Justin Huth <Jayhuth96@gmail.com>
2023-03-17 14:34:16 -0500

    Added instances for ingredents

--- GroceryListBuilder/Models/APICall.cs
+++ GroceryListBuilder/Models/APICall.cs
@@ -16,6 +16,17 @@ namespace APICall
             var instance = new Meal();
             instance.StrMeal = formattedResponse["meals"][0]["strMeal"].ToString();
             instance.StrInstructions = formattedResponse["meals"][0]["strInstructions"].ToString();
+            instance.StrIngredient1 = formattedResponse["meals"][0]["strIngredient1"].ToString();
+            instance.StrIngredient2 = formattedResponse["meals"][0]["strIngredient2"].ToString();
+            instance.StrIngredient3 = formattedResponse["meals"][0]["strIngredient3"].ToString();
+            instance.StrIngredient4 = formattedResponse["meals"][0]["strIngredient4"].ToString();
+            instance.StrIngredient5 = formattedResponse["meals"][0]["strIngredient5"].ToString();
+            instance.StrIngredient6 = formattedResponse["meals"][0]["strIngredient6"].ToString();
+            instance.StrIngredient7 = formattedResponse["meals"][0]["strIngredient7"].ToString();
+            instance.StrIngredient8 = formattedResponse["meals"][0]["strIngredient8"].ToString();
+            instance.StrIngredient9 = formattedResponse["meals"][0]["strIngredient9"].ToString();
+            instance.StrIngredient10 = formattedResponse["meals"][0]["strIngredient10"].ToString();
+ 
             return instance;
         }
     }
--- GroceryListBuilder/Models/Meal.cs
+++ GroceryListBuilder/Models/Meal.cs
@@ -4,6 +4,15 @@
     {
         public string StrMeal { get; set; }
         public string StrInstructions { get; set; }
-       
+        public string StrIngredient1 { get; set; }
+        public string StrIngredient2 { get; set; }
+        public string StrIngredient3 { get; set; }
+        public string StrIngredient4 { get; set; }
+        public string StrIngredient5 { get; set; }
+        public string StrIngredient6 { get; set; }
+        public string StrIngredient7 { get; set; }
+        public string StrIngredient8 { get; set; }
+        public string StrIngredient9 { get; set; }
+        public string StrIngredient10 { get; set; }
     }
 }
